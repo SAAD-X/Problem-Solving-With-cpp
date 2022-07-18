bc7f4a8c24d218220a117e9e7261d748ba31a0bf
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-07-18 17:52:33 +0600

    update

---
+++ Problem Solving in Codeforces/467A_George_and_Accommodation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, pi, pa, count = 0, temp;
+    cin >> i;
+    for(int j = 0; j < i; j++)
+    {
+        cin >> pi >> pa;
+    }
+    return 0;
+}

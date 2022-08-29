bd573d675e28b0e31ad22dba5514b9268af9c199
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-08-29 20:15:26 +0600

    References

---
+++ My Practise/References/memory_address.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name = "John";
+
+    cout<<&name;
+
+    return 0;
+}
---
+++ My Practise/References/ref1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string man = "aftab";
+    string &human = man;
+
+    cout<< human;
+
+    return 0;
+}
